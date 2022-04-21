1544897f99a3ba12dff23c93b680e342ce25e9fd
nmartorell <me@nedmartorell.com>
2022-04-21 22:18:32 +0000

    Edited files

--- resource/get_spark_base_images.py
+++ resource/get_spark_base_images.py
@@ -11,6 +11,4 @@ def do(payload, config, plugin_config, inputs):
         spark_image_tags.append(next(x for x in img.tags if "dataiku-dss-spark-exec-base" in x))
 
     choices = [{"label":tag, "value":tag} for tag in spark_image_tags]
-    
-    print(choices)
     return {"choices" : choices}
