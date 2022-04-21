402277d9c905073687c8c28299f33a3c6e6ba8fb
nmartorell <me@nedmartorell.com>
2022-04-21 22:42:44 +0000

    Edited files

--- python-runnables/build-shs-docker-image/runnable.py
+++ python-runnables/build-shs-docker-image/runnable.py
@@ -50,6 +50,8 @@ class MyRunnable(Runnable):
         tmp_folder = "/tmp/shs-docker-env/"
         try:
             shutil.rmtree(tmp_folder)
+        except:
+            pass
         finally:
             os.mkdir(tmp_folder)
         
