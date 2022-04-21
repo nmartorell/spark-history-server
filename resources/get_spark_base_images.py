baa298e9816fe14b5b1134a4bf03d548ca65300b
nmartorell <me@nedmartorell.com>
2022-04-21 22:14:13 +0000

    Edited files

--- resources/get_spark_base_images.py
+++ resources/get_spark_base_images.py
@@ -4,4 +4,11 @@ import docker
 def do(payload, config, plugin_config, inputs):
     
     # get docker client
+    docker_client = docker.from_env()
     
+    # get spark base images -- assumes spark images have been downloaded from our cdn
+    spark_image_tags = list()
+    for img in docker_client.images.list("dataiku-dss-spark-exec-base"):
+        spark_image_tags.append(next(x for x in img.tags if "dataiku-dss-spark-exec-base" in x))
+
+    return [{"label":tag, "value":tag} for tag in spark_image_tags]
