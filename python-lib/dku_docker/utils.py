12c3f0cdc8f80a2ff4640bb27b65997183bb532f
nmartorell <me@nedmartorell.com>
2022-04-22 20:49:25 +0000

    Edited file 'python-lib/dku_docker/utils.py'

--- python-lib/dku_docker/utils.py
+++ python-lib/dku_docker/utils.py
@@ -9,7 +9,7 @@ def generate_entrypoint_command(config):
         aws_secret_key = config["aws_secret_key"]
         s3_events_dir = config["s3_events_dir"]
         
-        command = 
+        command = "--{0} false {1} {2} --events-dir s3a://{3}".format(cloud_storage, s3_access_key, s3_secret_key, events_dir)
         
     elif cloud_storage == "wasbs":
         
