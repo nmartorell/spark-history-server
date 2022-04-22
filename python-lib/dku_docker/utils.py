d0f92f9bc690522d03f2273db41460b0aab22c02
nmartorell <me@nedmartorell.com>
2022-04-22 20:52:06 +0000

    Edited file 'python-lib/dku_docker/utils.py'

--- python-lib/dku_docker/utils.py
+++ python-lib/dku_docker/utils.py
@@ -15,9 +15,13 @@ def generate_entrypoint_command(config):
         
     elif cloud_storage == "wasbs":
         # extract config values 
-        
+        storage_account_name = config["storage_account_name"]
+        container_name = config["container_name"]
+        storage_account_key = config["storage_account_key"]
+        wasbs_events_dir = config["wasbs_events_dir"]
         
         # generate command
+        command = "--{0} false {1} {2} --events-dir s3a://{3}".format(cloud_storage, aws_access_key, aws_secret_key, s3_events_dir)
         
     elif cloud_storage == "gcs":
         raise Exception("unimplemented")
