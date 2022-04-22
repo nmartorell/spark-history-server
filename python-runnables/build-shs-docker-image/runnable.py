64c470f8a0c96c00e1f9f505f2f3d245e92c731c
nmartorell <me@nedmartorell.com>
2022-04-22 14:47:52 +0000

    Edited files

--- python-runnables/build-shs-docker-image/runnable.py
+++ python-runnables/build-shs-docker-image/runnable.py
@@ -73,7 +73,7 @@ class MyRunnable(Runnable):
         events_dir = "ned-martorell/shs"
         
         command = "--{0} false {1} {2} --events-dir s3a://{3}".format(cloud, s3_access_key, s3_secret_key, events_dir)
-        docker_client.containers.run(image=shs_image_obj.short_id[6:-1], 
+        docker_client.containers.run(image=shs_image_obj.tags[0], 
                                      ports={'18080/tcp': port},
                                      command=command)
         
