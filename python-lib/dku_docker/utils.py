35ee53fb46764d9e1f3644806cf4fffc8874debe
nmartorell <me@nedmartorell.com>
2022-04-22 20:47:24 +0000

    Edited file 'python-lib/dku_docker/utils.py'

--- python-lib/dku_docker/utils.py
+++ python-lib/dku_docker/utils.py
@@ -1,2 +1,14 @@
 def generate_entrypoint_command(config):
-    pass
+    """
+    Generate command for entrypoint based on cloud storage selected.
+    """
+    
+    cloud_storage = config["cloud_storage"]
+    if cloud_storage == "s3":
+        
+    elif cloud_storage == "wasbs":
+        
+    elif cloud_storage == "gcs":
+        
+    else:
+        raise Exception("this shouldn't happen")
