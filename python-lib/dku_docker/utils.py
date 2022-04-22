20745305919500893dc51869da3de298e98320ec
nmartorell <me@nedmartorell.com>
2022-04-22 20:48:17 +0000

    Edited file 'python-lib/dku_docker/utils.py'

--- python-lib/dku_docker/utils.py
+++ python-lib/dku_docker/utils.py
@@ -5,12 +5,14 @@ def generate_entrypoint_command(config):
     
     cloud_storage = config["cloud_storage"]
     if cloud_storage == "s3":
-        
+        aws_access_key = config["aws_access_key"]
+        aws_secret_key = config["aws_secret_key"]
+        s3_events_dir = 
         
         
     elif cloud_storage == "wasbs":
         
     elif cloud_storage == "gcs":
-        
+        raise Unimplemented
     else:
         raise Exception("this shouldn't happen.")
