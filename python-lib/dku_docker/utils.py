d5f5d1c0a4a1d4f47f4e321eed2bec1d4bd43e92
nmartorell <me@nedmartorell.com>
2022-04-22 20:50:39 +0000

    Edited file 'python-lib/dku_docker/utils.py'

--- python-lib/dku_docker/utils.py
+++ python-lib/dku_docker/utils.py
@@ -4,16 +4,24 @@ def generate_entrypoint_command(config):
     """
     
     cloud_storage = config["cloud_storage"]
-    if cloud_storage == "s3":
+    if cloud_storage == "s3":       
+        # extract config values
         aws_access_key = config["aws_access_key"]
         aws_secret_key = config["aws_secret_key"]
         s3_events_dir = config["s3_events_dir"]
         
-        command = "--{0} false {1} {2} --events-dir s3a://{3}".format(cloud_storage, s3_access_key, s3_secret_key, events_dir)
+        # generate command
+        command = "--{0} false {1} {2} --events-dir s3a://{3}".format(cloud_storage, aws_access_key, aws_secret_key, s3_events_dir)
         
     elif cloud_storage == "wasbs":
+        # extract config values 
+        
+        
+        # generate command
         
     elif cloud_storage == "gcs":
         raise Exception("unimplemented")
     else:
         raise Exception("This shouldn't happen... invalid Cloud Storage.")
+        
+    return command
